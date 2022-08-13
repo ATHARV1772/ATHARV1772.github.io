35730d045f720c264428e9746bd26516014d9678
ATHARV1772 <92803046+ATHARV1772@users.noreply.github.com>
2022-08-13 14:11:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Script running...")
+document.querySelector('.x').style.display='none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    if(document.querySelector('.sidebar').classList.contains('sidebargo')){
+        document.querySelector('.ham').style.display="inline"
+        document.querySelector('.x').style.display="none"
+    }
+    else{
+        document.querySelector('.ham').style.display="none"
+        setTimeout(()=>{
+            document.querySelector('.x').style.display="inline"
+        },300);
+        
+
+    }
+
+})
